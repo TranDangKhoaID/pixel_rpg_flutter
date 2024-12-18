8aabf259e16787356512dafc618378d0d63de16f
khoa.trandang2 <khoa.trandang2@di4l.vn>
2024-12-18 10:15:44 +0700

    first commit

---
+++ lib/my_game.dart
@@ -0,0 +1,34 @@
+import 'package:bonfire/bonfire.dart';
+import 'package:cool_game/globals.dart';
+import 'package:flutter/material.dart';
+
+class MyGame extends StatefulWidget {
+  const MyGame({super.key});
+
+  @override
+  State<MyGame> createState() => _MyGameState();
+}
+
+class _MyGameState extends State<MyGame> {
+  final _cameraConfig = CameraConfig(
+    initialMapZoomFit: InitialMapZoomFitEnum.fitHeight,
+    moveOnlyMapArea: true,
+  );
+  final _keyboard = Keyboard(
+    config: KeyboardConfig(
+      enable: true,
+    ),
+  );
+  @override
+  Widget build(BuildContext context) {
+    return BonfireWidget(
+      playerControllers: [_keyboard],
+      cameraConfig: _cameraConfig,
+      map: WorldMapBySpritefusion(
+        WorldMapReader.fromAsset(
+          Globals.map,
+        ),
+      ),
+    );
+  }
+}
